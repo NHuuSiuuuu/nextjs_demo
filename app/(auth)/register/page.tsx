5974c381e6895eb3b95b515d0e6b978dcb93dedd
NHuuSiuuuu <nguyenhung2k4gh@gmail.com>
2026-04-12 12:19:17 +0700

    router + darkmode

---
+++ app/(auth)/register/page.tsx
@@ -0,0 +1,11 @@
+import { ModeToggle } from "@/components/mode-tongle";
+import React from "react";
+
+export default function LoginPage() {
+  return (
+    <div>
+      <ModeToggle />
+      Đăng ký
+    </div>
+  );
+}
